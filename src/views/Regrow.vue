6cc7e6d3d4c82dd9d1272e0a4d0b696ad5a94a24
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2021-09-25 17:07:19 +0200

    spacing added

--- src/views/Regrow.vue
+++ src/views/Regrow.vue
@@ -6,4 +6,14 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+section {
+  width: 100%;
+  height: 8vh;
+}
+@media only screen and (min-width: 1024px) {
+  section {
+    height: 12vh;
+  }
+}
+</style>
