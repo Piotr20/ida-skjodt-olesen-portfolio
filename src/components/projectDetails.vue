5d742ca2ea8cac8b34fd476280f04db634ec977e
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2021-09-26 23:33:44 +0200

    regrow project developments and new component added

---
+++ src/components/projectDetails.vue
@@ -0,0 +1,90 @@
+<template>
+  <section>
+    <div class="client-details">
+      <h2>CLIENT</h2>
+    </div>
+    <div class="services-details">
+      <h2>SERVICES</h2>
+    </div>
+    <div class="tools-details">
+      <h2>USED TOOLS</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ProjectDetails",
+};
+</script>
+
+<style lang="scss" scoped>
+section {
+  width: 100%;
+  margin: 24px 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 24px;
+  div {
+    padding: 32px;
+    h2 {
+      text-transform: uppercase;
+      color: white;
+      font-size: 32px;
+      padding-bottom: 32px;
+      margin-bottom: 32px;
+      position: relative;
+      text-transform: uppercase;
+      &::after {
+        content: "";
+        position: absolute;
+        width: 50%;
+        height: 5px;
+        background-color: #fff;
+        left: 50%;
+        bottom: 0;
+        transform: translate(-50%, -50%);
+      }
+    }
+    p {
+      color: #95867d;
+      font-size: 18px;
+    }
+  }
+}
+.client-details {
+  background-color: #a093f0;
+}
+.services-details {
+  background-color: #e54575;
+}
+.tools-details {
+  background-color: #e877c1;
+}
+@media only screen and (min-width: 1024px) {
+  section {
+    margin: 36px 0;
+    gap: 36px;
+    div {
+      padding: 64px;
+      h2 {
+        font-size: 158px;
+        font-size: 48px;
+        padding-bottom: 48px;
+        margin-bottom: 48px;
+
+        text-align: left;
+        &::after {
+          width: 20%;
+          left: 0;
+          transform: translateX(0);
+        }
+      }
+      p {
+        font-size: 20px;
+        text-align: left;
+      }
+    }
+  }
+}
+</style>
--- src/views/Regrow.vue
+++ src/views/Regrow.vue
@@ -1,13 +1,39 @@
 <template>
   <div id="spacer"></div>
   <TextBox text="regrow" />
+  <section class="project-details">
+    <div>
+      <h2 class="">PROJECT DESCRIPTION</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nisl enim
+        pellentesque sodales id. Donec convallis maecenas viverra venenatis
+        tincidunt consectetur pulvinar dignissim. Elit lacus donec at auctor
+        commodo, urna auctor quam. Dolor consequat, eget elementum lacinia
+        eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+        nisl enim pellentesque sodales id. Donec convallis maecenas viverra
+        venenatis tincidunt consectetur pulvinar dignissim. Elit lacus donec at
+        auctor commodo, urna auctor quam. Dolor consequat, eget elementum
+        lacinia eleifend.
+      </p>
+    </div>
+    <div>
+      <img
+        src="../assets/images/regrow-images/hero-image-regrow.png"
+        alt="Regrow hero image"
+      />
+    </div>
+  </section>
+  <ProjectDetails />
 </template>
 
 <script>
 import TextBox from "../components/textBox.vue";
+import ProjectDetails from "../components/projectDetails.vue";
+
 export default {
   components: {
     TextBox,
+    ProjectDetails,
   },
 };
 </script>
@@ -17,9 +43,93 @@ export default {
   width: 100%;
   height: 8vh;
 }
+
+section.project-details {
+  width: 100%;
+  & > div:nth-of-type(1) {
+    background-color: #f1ebe7;
+    padding: 32px;
+  }
+  & > div:nth-of-type(2) {
+    img {
+      width: 100%;
+    }
+  }
+  h2 {
+    text-transform: uppercase;
+    color: #95867d;
+    font-size: 32px;
+    padding-bottom: 32px;
+    margin-bottom: 32px;
+    position: relative;
+    text-transform: uppercase;
+    &::after {
+      content: "";
+      position: absolute;
+      width: 50%;
+      height: 5px;
+      background-color: #95867d;
+      left: 50%;
+      bottom: 0;
+      transform: translate(-50%, -50%);
+    }
+  }
+  p {
+    color: #95867d;
+    font-size: 18px;
+  }
+}
 @media only screen and (min-width: 1024px) {
   #spacer {
     height: 12vh;
+  }
+  section.project-details {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 36px;
+
+    & > div:nth-of-type(1) {
+      padding: 64px;
+    }
+    & > div:nth-of-type(2) {
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+    h2 {
+      font-size: 158px;
+      font-size: 48px;
+      padding-bottom: 48px;
+      margin-bottom: 48px;
+
+      text-align: left;
+      &::after {
+        width: 10%;
+        left: 0;
+        transform: translateX(0);
+      }
+    }
+    p {
+      font-size: 20px;
+      text-align: left;
+    }
+  }
+}
+@media only screen and (min-width: 1440px) {
+  section.project-details {
+    h2 {
+      font-size: 158px;
+      font-size: 48px;
+      padding-bottom: 48px;
+      &::after {
+        width: 10%;
+      }
+    }
+    p {
+      font-size: 22px;
+    }
   }
 }
 </style>
