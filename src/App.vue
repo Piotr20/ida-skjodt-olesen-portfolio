b260b8d5901d773fd33e7a354b6730fe6d7e9113
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2021-10-05 21:38:07 +0200

    Font adjusted

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Gluten", cursive;
   text-align: center;
   color: #2c3e50;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -64,7 +64,6 @@ html {
   scroll-behavior: smooth;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
